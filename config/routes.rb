23086fe8af977e9f1c677cb7ef1b1ca5724606c8
Zack V <zacktwogo@gmail.com>
2019-11-20 04:11:19 +0000

    Sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-root 'static_pages#index'
-resources :courses, only: [:index, :show]
-namespace :instructor do
-  resources :courses, only: [:new, :create, :show]
- end
+  root 'static_pages#index'
+  resources :courses, only: [:index, :show]
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
+  end
 end
